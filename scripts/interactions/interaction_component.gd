aa5b16136b7e64338c510363aa2737cf8466b998
Yassine <yassineinfo500@gmail.com>
2025-08-05 15:03:51 +0100

    Added lever , wheel , item shader , interaction updates ,flashlight , started inventory system

--- scripts/interactions/interaction_component.gd
+++ scripts/interactions/interaction_component.gd
@@ -3,31 +3,43 @@ extends Node
 
 enum Interaction_Type {
 	DEFAULT,
-	DOOR
+	DOOR,
+	SWITCH,
+	WHEEL,
+	ITEM
 }
 
 @export var object_ref : Node3D
 @export var interaction_type : Interaction_Type = Interaction_Type.DEFAULT
 @export var pivot_point : Node3D
 @export var maximum_rotation : float = 90.0
+@export var nodes_to_affect : Array[Node]
 
 var can_interact :bool = true
 var is_interacting :bool = false
-
 var player_hand : Marker3D
-
 var lock_camera : bool = false
 var starting_rotation :float 
 var is_front : bool
+var camera : Camera3D
+var previous_mouse_pos : Vector2
+var wheel_rotation : float = 0.0
 
+# Signals
+signal item_collected(item:Node)
 
 func _ready() -> void:
 	match interaction_type :
 		Interaction_Type.DOOR :
 			starting_rotation = pivot_point.rotation.x
 			maximum_rotation = deg_to_rad(rad_to_deg(starting_rotation)+maximum_rotation)
-
-
+		Interaction_Type.SWITCH :
+			starting_rotation = object_ref.rotation.z
+			maximum_rotation = deg_to_rad(rad_to_deg(starting_rotation)+maximum_rotation) 
+		Interaction_Type.WHEEL :
+			starting_rotation = object_ref.rotation.x
+			maximum_rotation = deg_to_rad(rad_to_deg(starting_rotation)+maximum_rotation)
+			camera = get_tree().get_current_scene().find_child("Camera3D",true,false)
 #Run once , when the player FIRST clicks on an object
 func preInteract(hand : Marker3D) ->void:
 	is_interacting = true
@@ -36,6 +48,12 @@ func preInteract(hand : Marker3D) ->void:
 			player_hand = hand
 		Interaction_Type.DOOR :
 			lock_camera = true
+		Interaction_Type.SWITCH :
+			lock_camera = true
+		Interaction_Type.WHEEL :
+			lock_camera = true
+			previous_mouse_pos = get_viewport().get_mouse_position()
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE 
 #Run every frame perform some logic on this object
 func interact()->void:
 	if not can_interact:
@@ -43,6 +61,8 @@ func interact()->void:
 	match interaction_type:
 		Interaction_Type.DEFAULT:
 			_default_interact()
+		Interaction_Type.ITEM :
+			_collect_item()
  
 func auxInteract() -> void :
 	if not can_interact:
@@ -54,6 +74,7 @@ func auxInteract() -> void :
 func postInteract()->void:
 	is_interacting = false
 	lock_camera = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
 	if is_interacting:
@@ -65,6 +86,31 @@ func _input(event: InputEvent) -> void:
 					else :
 						pivot_point.rotate_y(event.relative.y * .001)
 					pivot_point.rotation.y = clamp(pivot_point.rotation.y , starting_rotation - maximum_rotation , maximum_rotation)
+			Interaction_Type.SWITCH :
+				if event is InputEventMouseMotion :
+					var percentage : float
+					object_ref.rotate_z(event.relative.y * .001)
+					object_ref.rotation.z = clamp(object_ref.rotation.z , starting_rotation , maximum_rotation)
+					percentage = (object_ref.rotation.z - starting_rotation) / (maximum_rotation - starting_rotation)
+					
+					notify_nodes(percentage)
+			Interaction_Type.WHEEL:
+				if event is InputEventMouseMotion:
+					var mouse_position : Vector2 = event.position
+					var cross = calculate_cross_product(mouse_position)
+
+					if cross < 0:
+						wheel_rotation += 0.2
+					else:
+						wheel_rotation -= 0.2
+
+					object_ref.rotation.x = clamp(wheel_rotation * 0.1, starting_rotation, maximum_rotation)
+
+					var percentage = (object_ref.rotation.x - starting_rotation) / (maximum_rotation - starting_rotation)
+					notify_nodes(percentage)
+
+					#  Update the previous mouse position
+					previous_mouse_pos = mouse_position
 
 func _default_interact() -> void :
 	var object_current_position : Vector3 = object_ref.global_transform.origin
@@ -93,3 +139,24 @@ func set_direction(_normal : Vector3) -> void :
 		is_front = true
 	else :
 		is_front = false
+
+
+func notify_nodes(percentage : float ) -> void :
+	for node in nodes_to_affect :
+		if node.has_method("execute") :
+			node.call("execute" , percentage)
+
+
+func calculate_cross_product(_mouse_position: Vector2) -> float:
+	var center_position = camera.unproject_position(object_ref.global_transform.origin)
+	var vector_to_previous = previous_mouse_pos - center_position
+	var vector_to_current = _mouse_position - center_position
+	var cross_product = vector_to_previous.x * vector_to_current.y - vector_to_previous.y * vector_to_current.x
+	
+	return cross_product
+	
+
+
+func _collect_item()-> void:
+	emit_signal("item_collected" , get_parent() )
+	get_parent().queue_free()
--- scripts/interactions/interaction_controller.gd
+++ scripts/interactions/interaction_controller.gd
@@ -4,19 +4,39 @@ extends Node
 @onready var interaction_raycast: RayCast3D = %interaction_raycast
 @onready var camera_3d: Camera3D = %Camera3D
 @onready var hand: Marker3D = %Hand
+@onready var default_reticle: TextureRect = %DefaultReticle
+@onready var highlight_reticle: TextureRect = %HighlightReticle
+@onready var interacting_reticle: TextureRect = %InteractingReticle
+@onready var interactable_check: Area3D = $"../InteractableCheck"
+
+@onready var outline_material : Material = preload("res://materials/outline.tres")
+
+
 
 
 var current_object : Object
 var last_potential_object : Object
 var interaction_component : Node
 
+func _ready() -> void:
+	interactable_check.body_entered.connect(_on_body_entered)
+	interactable_check.body_exited.connect(_on_body_exited)
+	#MAKE SURE CROSSHAIR IS ALWAYS CENTERED 
+	#default_reticle.position.x = get_viewport().size.x / 2
+	#default_reticle.position.x = get_viewport().size.y / 2
+
 func _process(delta: float) -> void:
+	if interaction_component and interaction_component.is_interacting :
+		default_reticle.visible = false
+		highlight_reticle.visible = false
+		interacting_reticle.visible = true
 	#if on previous frame we were interacting with an object , lets keep interacting  
 	if current_object :
 		if Input.is_action_just_pressed("secondary") :
 			if interaction_component :
 				interaction_component.auxInteract()
 				current_object = null
+				_unfocus()
 		elif Input.is_action_pressed("primary") :
 			if interaction_component :
 				interaction_component.interact()
@@ -24,6 +44,7 @@ func _process(delta: float) -> void:
 			if interaction_component :
 				interaction_component.postInteract()
 				current_object = null
+				_unfocus()
 	else :#we weren't interacting with something lets see if we can 
 		var potential_object : Object = interaction_raycast.get_collider()
 		
@@ -33,15 +54,53 @@ func _process(delta: float) -> void:
 				if interaction_component.can_interact == false:
 					return
 				last_potential_object = current_object
+				_focus()
 				if Input.is_action_just_pressed("primary") :
 					current_object = potential_object
+					if interaction_component.interaction_type == interaction_component.Interaction_Type.ITEM:
+						if not interaction_component.is_connected("item_collected", Callable(self, "_on_item_collected")):
+							interaction_component.connect("item_collected", Callable(self, "_on_item_collected"))
 					interaction_component.preInteract(hand)
-					
-					
+
+						
+						
 					if interaction_component.interaction_type == interaction_component.Interaction_Type.DOOR :
 						interaction_component.set_direction(current_object.to_local(interaction_raycast.get_collision_point()))
+		else :
+			_unfocus()
 func isCameraLocked() -> bool :
 	if interaction_component:
 		if interaction_component.lock_camera and interaction_component.is_interacting :
 			return true	
 	return false
+
+func _focus()->void :
+	default_reticle.visible = false
+	highlight_reticle.visible = true
+	interacting_reticle.visible = false
+
+
+func _unfocus()->void :
+	default_reticle.visible = true
+	highlight_reticle.visible = false
+	interacting_reticle.visible = false
+
+
+func _on_item_collected(item: Node) :
+	#INVETORY SYSTEME ?
+	print("added to inventory (not really)  :  ", item)
+
+func _on_body_entered(body : Node3D) -> void : 
+	if body.name != "Player" :
+		var name = body.name
+		var ic = body.get_node_or_null("InteractionComponent")
+		if ic and ic.interaction_type == ic.Interaction_Type.ITEM : 
+			var mesh : MeshInstance3D = body.find_child("MeshInstance3D" , true , false)
+			mesh.material_overlay = outline_material
+
+
+func _on_body_exited(body : Node3D) -> void :
+	if body.name != "Player" :
+		var mesh : MeshInstance3D = body.find_child("MeshInstance3D" , true , false)
+		if mesh :
+			mesh.material_overlay = null
